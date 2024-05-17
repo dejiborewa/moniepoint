6b5873e53478ff9eca2939f8adbd0a443de6d9e5
Ayodeji Akinborewa <ayodejiakinborewa@Z.local>
2024-05-17 18:29:21 +0100

    fix: change big text color

--- src/components/data/data.tsx
+++ src/components/data/data.tsx
@@ -2,8 +2,8 @@ import Menu from "../menu/menu";
 
 const Data = () => {
   return (
-    <section className="bg-offWhite h-[135vh] -mx-6 rounded-[100px] pl-10 py-20  2xl:pl-[7.5rem]">
-      <div className="flex overflow-x-hidden">
+    <section className="bg-offWhite h-[145vh] -mx-6 rounded-[100px] pl-10 py-20 2xl:pl-[7.5rem]">
+      <div className="flex overflow-x-hidden relative top-[15vh] 2xl:top-[20vh]">
         <div className="w-[50%] 2xl:w-[40%]">
           <div className="text-[80px] leading-[6rem]">
             <h1>
@@ -20,12 +20,22 @@ const Data = () => {
         </div>
       </div>
 
-      <div className="h-max flex items-baseline font-semibold 2xl:tracking-[-1.5em] relative -top-[10vh]">
-        <h1 className="lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">R</h1>
-        <h1 className="lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">a</h1>
-        <h1 className="lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">m</h1>
-        <h1 className="lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">o</h1>
-        <h1 className="lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">s</h1>
+      <div className="h-max flex items-baseline font-semibold relative tracking-[-1.5em] 2xl:tracking-[-2em] text-orange">
+        <h1 className="text-[100px] lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">
+          R
+        </h1>
+        <h1 className="text-[100px] lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">
+          a
+        </h1>
+        <h1 className="text-[100px] lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">
+          m
+        </h1>
+        <h1 className="text-[100px] lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">
+          o
+        </h1>
+        <h1 className="text-[100px] lg:text-[300px] xl:text-[450px] 2xl:text-[620px]">
+          s
+        </h1>
       </div>
     </section>
   );
